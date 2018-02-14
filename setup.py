209e097d538faa9fd76ce752607f05600ce0adac
Ethan Wessel <ejwessel@gmail.com>
2018-02-13 22:59:25 -0800

    add in install commands

---
+++ setup.py
@@ -0,0 +1,6 @@
+import subprocess
+
+subprocess.call("apt-get install vim -y", shell=True)
+subprocess.call("apt-get install tmux -y", shell=True)
+subprocess.call("apt-get install ansible -y", shell=True)
+
