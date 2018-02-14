1fb84cab181ebb1c5c1e4869f201f37d54060336
Ethan Wessel <ejwessel@gmail.com>
2018-02-13 23:31:26 -0800

    basic arguments

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 import subprocess
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--skip_ansible_install",
+                        action="store_false",
+                        help="allows skipping of ansible install")
+parser.add_argument("-c", "--currency_type",
+                        required=True,
+                        help="bitcoin, ethereum, litecoin")
+parser.add_argument("--smtp",
+                        help="if you have the smpt server, port, username, and password you can set up alerting")
+args = parser.parse_args()
 
 #Install ansible. Allows the running of the ansible script
-subprocess.call("apt-get update", shell=True)
-subprocess.call("apt-get upgrade -y", shell=True)
-subprocess.call("apt-get install ansible -y", shell=True)
+if (args.skip_ansible_install):
+  subprocess.call("apt-get update", shell=True)
+  subprocess.call("apt-get upgrade -y", shell=True)
+  subprocess.call("apt-get install ansible -y", shell=True)
 
 #Run the ansible commands to set up the node
+print args.currency_type
+print args.setup_smtp
