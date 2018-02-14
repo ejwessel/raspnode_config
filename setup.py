ff982dcbf2ea083ea95d3f045e4d0d0af3dfed85
Ethan Wessel <ejwessel@gmail.com>
2018-02-13 23:05:20 -0800

    apt-get update and apt-get upgrade

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import subprocess
 
+subprocess.call("apt-get update", shell=True)
+subprocess.call("apt-get upgrade -y", shell=True)
 subprocess.call("apt-get install vim -y", shell=True)
 subprocess.call("apt-get install tmux -y", shell=True)
 subprocess.call("apt-get install ansible -y", shell=True)
