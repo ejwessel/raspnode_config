94926a70e8050ad4bc0ef1b56da4e362945496fb
Ethan Wessel <ejwessel@gmail.com>
2018-02-13 23:14:31 -0800

    comments and come fixes

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import subprocess
 
+#Install ansible. Allows the running of the ansible script
 subprocess.call("apt-get update", shell=True)
 subprocess.call("apt-get upgrade -y", shell=True)
-subprocess.call("apt-get install vim -y", shell=True)
-subprocess.call("apt-get install tmux -y", shell=True)
 subprocess.call("apt-get install ansible -y", shell=True)
 
+#Run the ansible commands to set up the node
