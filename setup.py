60f1071265fde9fe41745c14d97747b1309c771f
Ethan Wessel <ejwessel@gmail.com>
2018-02-14 00:04:51 -0800

    additional parameters and running of ansible command

--- setup.py
+++ setup.py
@@ -2,15 +2,18 @@ import subprocess
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--skip_ansible_install",
+parser.add_argument("--skip-ansible-install",
                         action = "store_false",
                         help = "allows skipping of ansible install")
-parser.add_argument("-c", "--currency_type",
+parser.add_argument("-c", "--currency-type",
                         required = True,
                         help="bitcoin, ethereum, litecoin")
 parser.add_argument("--smtp",
                         action = "store_true",
                         help = "if you have the smpt server, port, username, and password you can set up alerting")
+parser.add_argument("-d", "--dry-run",
+                       action = "store_true",
+                       help = "dry-run ansible playbook")
 args = parser.parse_args()
 
 #Install ansible. Allows the running of the ansible script
@@ -20,7 +23,10 @@ if (args.skip_ansible_install):
   subprocess.call("apt-get install ansible -y", shell=True)
 
 #Run the ansible commands to set up the node
+dry_run = ""
+if (args.dry_run):
+  dry_run = "--check"
 
-print args.currency_type
-if (args.smtp is not None):
-  print args.smtp
+command = 'ansible-playbook -v -i "localhost," --connection local playbook.yml %s --extra-vars "currency_type=%s setup_smtp=%s"' % (dry_run, args.currency_type, str(args.smtp))
+print command
+subprocess.call(command, shell=True)
